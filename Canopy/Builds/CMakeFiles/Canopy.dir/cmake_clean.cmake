10d55445088743fadecbb937667c5124f7bd6bb4
Philip Beckwith <philip.beckwith@ars.usda.gov>
2018-08-06 14:40:40 -0700

    Adding Canopy.

---
+++ Canopy/Builds/CMakeFiles/Canopy.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Canopy.dir/Canopy.cpp.o"
+  "Canopy.pdb"
+  "Canopy"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Canopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
